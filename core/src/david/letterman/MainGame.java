b51205eef52795f6971ae80e539d7cea15b16a2e
David Wigley <dwigley92@gmail.com>
2016-06-21 20:47:10 -0400

    bigger buttons

--- core/src/david/letterman/MainGame.java
+++ core/src/david/letterman/MainGame.java
@@ -16,42 +16,71 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton.ImageButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 public class MainGame extends ApplicationAdapter {
 
+	SpriteBatch batch;
+	//texture of our man
+	private Texture manText;
+	//music for test purposes
+	private Music testMusic;
+	//rectangle object for our man texture, may have to change in future
+	private Rectangle man;
+	//Orthographic camera
 	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private Stage stage; //** stage holds the Button **//
-	private TextureAtlas buttonsAtlas; //** image of buttons **//
-	private Skin buttonSkin; //** images are used as skins of the button **//
-	private ImageButton button; //** the button - the only actor in program **//
+
+	//Dave
+	Stage stage;
+	ImageButton button;
+	Skin skin;
+	TextureAtlas buttonAtlas;
+
+	ImageButton.ImageButtonStyle imageButtonStyle;
 
 	@Override
-	public void create() {
+	public void create () {
+//		//set texture for man
+		manText = new Texture(Gdx.files.internal("Letterman.png"));
+
+		//set music
+		//testMusic = Gdx.audio.newMusic("stuff.mp3");
+		//starts playing music on loop
+		//testMusic.setLooping(true);
+		//testMusic.play();
+		//set camera
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480); //** w/h ratio = 1.66 **//
+		//800x480 is the resolution of SOME android devices
+		//subject to change if we implement resolution scaling
+		camera.setToOrtho(false, 800, 480);
+
+		//set man, he is centered as of right now and his height and width
+		//are arbitrary and subject to change
+		man = new Rectangle();
+		man.x = 400;
+		man.y = 240;
+		//changes based on png
+		man.width = 64;
+		man.height = 64;
 
 		batch = new SpriteBatch();
 
-		buttonsAtlas = new TextureAtlas(Gdx.files.internal("buttons/buttons.pack")); //** button atlas image **//
-		buttonSkin = new Skin();
-		buttonSkin.addRegions(buttonsAtlas); //** skins for on and off **//
 
-		stage = new Stage();        //** window is stage **//
+		stage = new Stage();
 		stage.clear();
-		Gdx.input.setInputProcessor(stage); //** stage is responsive **//
-
-		ImageButtonStyle style = new ImageButtonStyle(); //** Button properties **//
-		style.up = buttonSkin.getDrawable("Arrow Left Down");
-		style.down = buttonSkin.getDrawable("Arrow Left Up");
-
-		button = new ImageButton(style); //** Button text and style **//
-		button.setPosition(100, 100); //** Button location **//
-		button.setHeight(300); //** Button Height **//
-		button.setWidth(600); //** Button Width **//
+		Gdx.input.setInputProcessor(stage);
+		skin = new Skin();
+		buttonAtlas = new TextureAtlas(Gdx.files.internal("buttons/buttons.pack"));
+		skin.addRegions(buttonAtlas);
+		imageButtonStyle = new ImageButton.ImageButtonStyle();
+		imageButtonStyle.up = skin.getDrawable("right up large");
+		imageButtonStyle.down = skin.getDrawable("down right large");
+		button = new ImageButton(imageButtonStyle);
+		button.setPosition(200, 100); //** Button location **//
+		button.setHeight(181); //** Button Height **//
+		button.setWidth(640); //** Button Width **//
+		stage.addActor(button);
 		button.addListener(new InputListener() {
 			public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
 				Gdx.app.log("my app", "Pressed"); //** Usually used to start Game, etc. **//
@@ -62,41 +91,55 @@ public class MainGame extends ApplicationAdapter {
 				Gdx.app.log("my app", "Released");
 			}
 		});
-
-		stage.addActor(button);
 	}
 
 	@Override
-	public void dispose() {
-		batch.dispose();
-		buttonSkin.dispose();
-		buttonsAtlas.dispose();
-		stage.dispose();
-	}
+	/*
+	 *  Method called by the game loop from the application every time rendering should be performed.
+	 *  Game logic updates are usually also performed in this method.
+	 */
+	public void render () {
+		super.render();
 
-	@Override
-	public void render() {
+//		//initializes white background
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
 		stage.act();
+		stage.draw();
+		//updates camera
+		camera.update();
 
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		stage.draw();
+		batch.draw(manText, man.x, man.y);
 		batch.end();
+		if(Gdx.input.isTouched()) {
+			Vector3 touchPos = new Vector3();
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			man.x = touchPos.x;
+			man.y = touchPos.y;
+		}
+
+	}
+	public void resize (int width, int height) {
 	}
 
-	@Override
-	public void resize(int width, int height) {
+	/*
+	 * On Android this method is called when the Home button is pressed or an incoming call is received.
+	 * On desktop this is called just before dispose() when exiting the application.
+	 * A good place to save the game state.
+	 */
+	public void pause () {
 	}
 
-	@Override
-	public void pause() {
+	public void resume () {
 	}
 
-	@Override
-	public void resume() {
+	public void dispose () {
+		stage.dispose();
+		manText.dispose();
+		batch.dispose();
 	}
 
 }
